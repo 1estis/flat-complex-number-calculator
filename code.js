b8b5e12466cbec5965cc0d3d535ac5611264d352
Estis <kulaykin2000@gmail.com>
2023-08-28 23:20:26 +0600

    All features work a little

--- code.js
+++ code.js
@@ -1,13 +1,19 @@
 const scale_step = 1.5; // Zoom step
-const grid_size_default = 35; // Default grid size (px)
-const grid_size_reset_step = 2; // Reset grid size when grid size * grid_size_reset <= 1 or grid size / grid_size_reset >= 1
+const base_interval_size = 200; // Base grid step size (px)
+const default_interval = 20; // Default interval between grid steps (units)
 
-var canvas = document.getElementById("my-canvas");
-var ctx = canvas.getContext("2d");
+const beground_color = "#ffffff"; // Background color
+const grid_mainline_color = "#000000"; // Main grid lines color
+const grid_subline_color = "#d3d3d3"; // Sub grid lines color
+const grid_center_color = "#000000"; // Grid center color
+const grid_number_color = "#000000"; // Grid numbers color
 
-var scale = 1; // Scale of grid
-var grid_size = grid_size_default;
-var grid_center_shift = { x: 0, y: 0 };
+let interval = default_interval; // Grid units per base_interval_size (grid units)
+let grid_center_shift = { x: 0, y: 0 }; // Grid center shift (grid units)
+
+
+let canvas = document.getElementById("my-canvas");
+let ctx = canvas.getContext("2d");
 
 // Resize canvas to fit window
 function resize_canvas() {
@@ -21,67 +27,104 @@ document.body.onresize = resize_canvas;
 // Mouse move event
 document.body.onmousemove = function (e) {
     if (e.buttons == 1) {
-        grid_center_shift.x += e.movementX;
-        grid_center_shift.y += e.movementY;
+        grid_center_shift.x += e.movementX / base_interval_size * interval;
+        grid_center_shift.y -= e.movementY / base_interval_size * interval;
         draw_grid();
     }
 };
 
 // Mouse wheel event
 document.body.onwheel = function (e) {
-    // Define mouse position on grid
-    var mouse_pos = {
-        x: (e.clientX - grid_center_shift.x) / scale,
-        y: (e.clientY - grid_center_shift.y) / scale
+    // Define mouse position on grid (grid units)
+    let mouse_pos = {
+        x: (e.clientX - canvas.width / 2) / (base_interval_size * interval) + grid_center_shift.x,
+        y: (canvas.height / 2 - e.clientY) / (base_interval_size * interval) + grid_center_shift.y
     };
 
-}
+    // Zoom in
+    if (e.deltaY < 0) {
+        if (interval > 1) interval -= 1;
+        else if (interval <= 1) interval -= 0.1;
+        if (interval <= 0) interval = 1;
+    }
+
+    // Zoom out
+    if (e.deltaY > 0) {
+        interval += 1;
+    }
+
+    draw_grid();
+};
 
 
 // Draw the grid
 function draw_grid() {
     
-    var canvas_width = canvas.clientWidth;
-    var canvas_height = canvas.clientHeight;
+    let canvas_width = canvas.clientWidth;
+    let canvas_height = canvas.clientHeight;
 
     // Clear canvas
     ctx.clearRect(0, 0, canvas_width, canvas_height);
 
-    // Canvas center
-    var canvas_center = {
+    // Canvas center (px)
+    let canvas_center = {
         x: canvas_width / 2,
         y: canvas_height / 2
     };
 
-
-    // Grid center (by default it is canvas center)
-    var grid_center = {
-        x: canvas_center.x + grid_center_shift.x,
-        y: canvas_center.y + grid_center_shift.y
+    // Grid center (px)
+    let grid_center = {
+        x: canvas_center.x + grid_center_shift.x * base_interval_size / interval,
+        y: canvas_center.y - grid_center_shift.y * base_interval_size / interval
     };
 
-    var x_lines_shift = grid_center.x % grid_size;
-    var y_lines_shift = grid_center.y % grid_size;
-    var num_lines_x = (canvas_width-x_lines_shift)/grid_size;
-    var num_lines_y = (canvas_height-y_lines_shift)/grid_size;
+    // Calculate ui_intervals
+    let ui_intervals = [
+        Math.pow(10, Math.floor(Math.log10(interval))),
+        Math.pow(10, Math.floor(Math.log10(interval/2)))*2,
+        Math.pow(10, Math.floor(Math.log10(interval/5)))*5
+    ];
 
-    // Draw all X-axis sublines in light gray (1px)
+    let ui_interval, ui_interval_size;
+    for (let i = 0; i < ui_intervals.length; i++) {
+        let curr_size = base_interval_size * ui_intervals[i] / interval;
+        if (i == 0) {
+            ui_interval = ui_intervals[i];
+            ui_interval_size = curr_size;
+        } else {
+            if (Math.abs(curr_size - base_interval_size) < Math.abs(ui_interval_size - base_interval_size)) {
+                ui_interval = ui_intervals[i];
+                ui_interval_size = curr_size;
+            }
+        }
+    }
+
+    console.log(interval, ui_interval, ui_interval_size);
+
+    let x_lines_shift = grid_center.x % ui_interval_size;
+    let y_lines_shift = grid_center.y % ui_interval_size;
+    let num_lines_x = (canvas_width-x_lines_shift)/ui_interval_size;
+    let num_lines_y = (canvas_height-y_lines_shift)/ui_interval_size;
+
+    // Draw all X-axis lines in light gray (1px)
     ctx.strokeStyle = "#d3d3d3";
     ctx.lineWidth = 1;
     
-    for (var i = 0; i < num_lines_x; i++) {
-        var x = x_lines_shift + i * grid_size;
-        if (x == grid_center.x) var x_main_line = x;
+    let x_main_line = undefined;
+    for (let i = 0; i < num_lines_x; i++) {
+        let x = x_lines_shift + i * ui_interval_size;
+        if (x == grid_center.x) x_main_line = x;
         ctx.beginPath();
         ctx.moveTo(x, 0);
         ctx.lineTo(x, canvas_height);
         ctx.stroke();
     }
     
-    // Draw all Y-axis sublines in light gray (1px)
-    for (var i = 0; i < num_lines_y; i++) {
-        var y = y_lines_shift + i * grid_size;
-        if (y == grid_center.y) var y_main_line = y;
+    // Draw all Y-axis lines in light gray (1px)
+    let y_main_line = undefined;
+    for (let i = 0; i < num_lines_y; i++) {
+        let y = y_lines_shift + i * ui_interval_size;
+        if (y == grid_center.y) y_main_line = y;
         ctx.beginPath();
         ctx.moveTo(0, y);
         ctx.lineTo(canvas_width, y);
@@ -109,12 +152,12 @@ function draw_grid() {
     }
 
     // Draw X-axis numbers (12px, on the left side of the grid center numbers are negative)
-    for (var i = 0; i < num_lines_x; i++) {
-        var x = x_lines_shift + i * grid_size;
-        var y = grid_center.y - 10;
+    for (let i = 0; i < num_lines_x; i++) {
+        let x = x_lines_shift + i * ui_interval_size;
+        let y = grid_center.y - 10;
         if (x == grid_center.x) continue;
-        var num = (x - grid_center.x) / grid_size * scale;
-        var num_str = num.toString();
+        let num = (x - grid_center.x) / ui_interval_size * ui_interval;
+        let num_str = num.toString();
         ctx.font = "12px Arial";
         ctx.fillStyle = "#000000";
         ctx.textBaseline = "middle";
@@ -142,12 +185,12 @@ function draw_grid() {
     }
 
     // Draw Y-axis numbers (12px, on the top side of the grid center numbers are negative)
-    for (var i = 0; i < num_lines_y; i++) {
-        var x = grid_center.x + 4;
-        var y = y_lines_shift + i * grid_size;
+    for (let i = 0; i < num_lines_y; i++) {
+        let x = grid_center.x + 4;
+        let y = y_lines_shift + i * ui_interval_size;
         if (y == grid_center.y) continue;
-        var num = (grid_center.y - y) / grid_size * scale;
-        var num_str = num.toString();
+        let num = (grid_center.y - y) / ui_interval_size * ui_interval;
+        let num_str = num.toString();
         ctx.font = "12px Arial";
         ctx.fillStyle = "#000000";
         ctx.textBaseline = "middle";
@@ -187,8 +230,6 @@ function draw_grid() {
     ctx.fillText("0", grid_center.x + 4, grid_center.y - 10);        
 }
 
-canvas.width = document.body.clientWidth;
-canvas.height = document.body.clientHeight;
-draw_grid();
+resize_canvas()
 
 
